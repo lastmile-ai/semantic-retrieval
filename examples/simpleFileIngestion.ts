4d1c1e405d3dd8c3010d16a878cb42c2d3646f8b
Ryan Holinshead <>
2023-09-13 16:39:49 -0400

    WIP Starting Point for Semantic Retrival Examples

---
+++ examples/simpleFileIngestion.ts
@@ -0,0 +1,26 @@
+// TODO: These imports should be from actual lastmile retrieval package
+import { AlwaysAllowDocumentAccessPolicyFactory } from '../src/access-control/alwaysAllowDocumentAccessPolicyFactory';
+import { InMemoryDocumentMetadataDB } from '../src/document/metadata/InMemoryDocumentMetadataDB';
+import { FileSystem } from '../src/ingestion/data-sources/dataSource';
+
+async function createIndex() {
+    const fileSystem = new FileSystem('./example_docs');
+    const rawDocuments = await fileSystem.loadDocuments();
+    const parsedDocuments = await SimpleDocumentParser.parseDocuments(rawDocuments, {
+        metadataDB: new InMemoryDocumentMetadataDB(),
+        accessControlPolicyFactory: new AlwaysAllowDocumentAccessPolicyFactory(),
+    });
+    return VectorDB.fromDocuments(parsedDocuments);
+}
+
+async function main() {
+    const vectorDB = await createIndex();
+    const query = "How do I use parameters in a workbook?";
+    // TODO: Should pass acl context to the vector DB for querying
+    const res = await vectorDB.query(query);
+    console.log(res);
+}
+
+main();
+
+export {};
---
+++ src/access-control/alwaysAllowDocumentAccessPolicyFactory.ts
@@ -0,0 +1,13 @@
+import { RawDocument } from "../document/document";
+import { DocumentAccessPolicyFactory } from "./documentAccessPolicyFactory";
+import { AlwaysAllowAccessPolicy } from "./policies/alwaysAllowAccessPolicy";
+import { ResourceAccessPolicy } from "./resourceAccessPolicy";
+
+
+export class AlwaysAllowDocumentAccessPolicyFactory implements DocumentAccessPolicyFactory {
+    constructor() {}
+
+    async  getAccessPolicies(_rawDocument: RawDocument): Promise<ResourceAccessPolicy[]> {
+        return [new AlwaysAllowAccessPolicy()];
+    }
+}
