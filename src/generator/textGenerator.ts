6a3dc4d0fb4ae901cecb047788811757e6814406
Ryan Holinshead <>
2023-09-25 17:35:13 -0400

    [RFC] Add Generator for Handling Final Piece of RAG
    The final piece of RAG ('generation') should also be handled in our semantic retrieval library. Ideally, we want to support some non-trivial generations, including using prompt templates and for completion and performing response synthesis and validation.
    
    To do so, this PR adds a generator base class with `run` method. Custom classes can implement their own logic for what 'generation' means

---
+++ src/generator/textGenerator.ts
@@ -0,0 +1,9 @@
+import { GeneratorParams } from "./generator";
+import { Document } from "../document/document";
+
+/**
+ * Simple abstract class for generating text from a prompt and retrieved Documents.
+ */
+export abstract class TextGenerator {
+  protected abstract run(params: GeneratorParams<Document[]>): Promise<string>;
+}
