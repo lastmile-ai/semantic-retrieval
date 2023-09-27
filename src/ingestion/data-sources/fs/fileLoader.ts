dc37007ef7d18dd5fbaa4dd86241f772e78e6259
Ryan Holinshead <>
2023-09-27 18:40:05 -0400

    Default FileSystem Data Source Implementation
    # Default FileSystem Data Source Implementation
    
    A basic default implementation for loading data from files (and directories of files) on disk. The implementation sets up a default mapping of file extensions to file loaders. Our default file loaders are just thin wrappers around the equivalent LangChain file loaders and can be configured using a param to the constructor

---
+++ src/ingestion/data-sources/fs/fileLoader.ts
@@ -0,0 +1,14 @@
+import { RawDocumentChunks } from "../../../document/document";
+
+/**
+ * Abstract class for loading chunked content from a file.
+ */
+export abstract class BaseFileLoader {
+    path: string;
+  
+    constructor(path: string) {
+      this.path = path;
+    }
+  
+    abstract loadChunkedContent(): Promise<RawDocumentChunks>;
+  }
