37f29535442800b2e356e52d85b96c4d6bee261c
Ryan Holinshead <>
2023-10-06 14:13:16 -0400

    Embeddings

---
+++ src/transformation/embeddings/openAIEmbedding.ts
@@ -0,0 +1,160 @@
+import {
+  DocumentFragmentEmbeddingsTransformer,
+  DocumentFragmentVectorEmbedding,
+} from "./embeddings";
+import { Document, DocumentFragment } from "../../document/document";
+import { type ClientOptions as OpenAIClientOptions, OpenAI } from "openai";
+import { JSONObject } from "../../common/jsonTypes";
+import { TiktokenModel, encoding_for_model } from "@dqbd/tiktoken";
+import { requestWithThrottleBackoff } from "../../utils/axiosUtils";
+
+interface OpenAIEmbeddingsConfig extends OpenAIClientOptions {
+  apiKey?: string;
+}
+
+type EmbedFragmentData = {
+  fragmentId: string;
+  text: string;
+};
+
+/**
+ * Transforms Documents into VectorEmbeddings using OpenAI's embedding model API. Each
+ * fragment of a Document is embedded as a separate VectorEmbedding and should not exceed
+ * the embedding model's max input tokens (8191 for text-embedding-ada-002).
+ */
+export class OpenAIEmbeddings extends DocumentFragmentEmbeddingsTransformer {
+  // For now, only support the text-embedding-ada-002 model to allow optimized batching.
+  // There is no clear information on / tokenization support for other embedding models
+  model = "text-embedding-ada-002" as TiktokenModel;
+
+  private client: OpenAI;
+
+  constructor(config?: OpenAIEmbeddingsConfig) {
+    super();
+
+    const apiKey = config?.apiKey ?? process.env.OPENAI_API_KEY;
+    if (!apiKey) {
+      throw new Error("No OpenAI API key found for OpenAIEmbeddings");
+    }
+
+    this.client = new OpenAI({ ...config, apiKey });
+  }
+
+  async embedFragment(
+    fragment: DocumentFragment
+  ): Promise<DocumentFragmentVectorEmbedding> {
+    const fragmentData = {
+      fragmentId: fragment.fragmentId,
+      text: await fragment.getContent(),
+    };
+    return (await this.createEmbeddings([fragmentData]))[0];
+  }
+
+  async transformDocuments(
+    documents: Document[]
+  ): Promise<DocumentFragmentVectorEmbedding[]> {
+    const embeddings: DocumentFragmentVectorEmbedding[] = [];
+
+    // We'll send the actual requests in batches of 5 in case there are lots of large documents
+    let requestBatch: EmbedFragmentData[][] = [];
+    const REQUEST_BATCH_SIZE = 5;
+
+    let currentTextBatch: EmbedFragmentData[] = [];
+    let currentTextBatchSize = 0;
+    let documentIdx = 0;
+
+    const encoding = encoding_for_model(this.model);
+
+    // openai supports batch embedding creation by sending a list of texts to embed in a
+    // single request, as long as the total length does not exceed the max input tokens
+    // for the model (8191 for text-embedding-ada-002). So, here we batch all fragment
+    // text across all documents to optimize individual requests in batches of fragment texts.
+    while (documentIdx < documents.length) {
+      const currentDocument = documents[documentIdx];
+
+      // TODO: Revisit if this is problematic for large documents; we can always move the
+      // fragment getContent logic into a loop below (at the cost of more code complexity)
+      const currentDocumentFragmentData = await Promise.all(
+        currentDocument.fragments.map(async (fragment) => ({
+          fragmentId: fragment.fragmentId,
+          text: await fragment.getContent(),
+        }))
+      );
+
+      let currentDocumentFragmentIdx = 0;
+
+      while (currentDocumentFragmentIdx < currentDocumentFragmentData.length) {
+        const currentFragmentData =
+          currentDocumentFragmentData[currentDocumentFragmentIdx];
+        const currentDocumentFragmentEncoding = encoding.encode(
+          currentFragmentData.text
+        );
+
+        // TODO: Handle this for other models when they are supported
+        if (currentDocumentFragmentEncoding.length > 8191) {
+          throw new Error(
+            `Fragment text exceeds max input tokens for model ${this.model} (8191 for text-embedding-ada-002)`
+          );
+        }
+
+        if (
+          currentTextBatchSize + currentDocumentFragmentEncoding.length >
+          8191
+        ) {
+          requestBatch.push(currentTextBatch);
+
+          if (requestBatch.length === REQUEST_BATCH_SIZE) {
+            const embeddingPromises = requestBatch.map((batch) =>
+              this.createEmbeddings(batch)
+            );
+            embeddings.push(...(await Promise.all(embeddingPromises)).flat());
+            currentTextBatch = [];
+            currentTextBatchSize = 0;
+            requestBatch = [];
+          }
+        }
+
+        currentTextBatch.push(currentFragmentData);
+        currentTextBatchSize += currentDocumentFragmentEncoding.length;
+        currentDocumentFragmentIdx++;
+      }
+
+      documentIdx++;
+    }
+
+    // Handle remaining requests after all documents have been processed
+    if (requestBatch.length > 0) {
+      const embeddingPromises = requestBatch.map((batch) =>
+        this.createEmbeddings(batch)
+      );
+      embeddings.push(...(await Promise.all(embeddingPromises)).flat());
+    }
+
+    encoding.free();
+    return embeddings;
+  }
+
+  private async createEmbeddings(
+    fragments: EmbedFragmentData[]
+  ): Promise<DocumentFragmentVectorEmbedding[]> {
+    const input = fragments.map((fragment) => fragment.text);
+    const embeddingRes = await requestWithThrottleBackoff(() =>
+      this.client.embeddings.create({
+        input,
+        model: this.model,
+      })
+    );
+    const { data, ...metadata } = embeddingRes;
+    return data.map((embedding, idx) => ({
+      fragmentId: fragments[idx].fragmentId,
+      vector: embedding.embedding,
+      metadata: {
+        ...metadata,
+        model: this.model,
+        // usage can't be typed as JSONObject since its keys are static in the interface
+        usage: metadata.usage as unknown as JSONObject,
+      },
+      attributes: {},
+    }));
+  }
+}
