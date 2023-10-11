10d9bdab3f847b8847d474d073456fed2c6cbc2f
Ryan Holinshead <>
2023-10-11 17:10:23 -0400

    Pinecone Vector DB

---
+++ __tests__/data-store/vector-DBs/pineconeVectorDB.test.ts
@@ -0,0 +1,186 @@
+import {
+  Pinecone,
+  QueryOptions,
+  RecordMetadata,
+  ScoredPineconeRecord,
+} from "@pinecone-database/pinecone";
+import getEnvVar from "../../../src/utils/getEnvVar";
+import { PineconeVectorDB } from "../../../src/data-store/vector-DBs/pineconeVectorDB";
+import { InMemoryDocumentMetadataDB } from "../../../src/document/metadata/InMemoryDocumentMetadataDB";
+import {
+  TEST_VECTOR,
+  TestEmbeddings,
+} from "../../__mocks__/transformation/embeddings/TestEmbeddings";
+import {
+  getTestDocument,
+  getTestDocumentFragment,
+} from "../../utils/testDocumentUtils";
+import { v4 as uuid } from "uuid";
+
+jest.mock("../../../src/utils/getEnvVar");
+jest.mock("uuid");
+
+function mockPineconeQuery(options: QueryOptions): Promise<{
+  matches?: Array<ScoredPineconeRecord<RecordMetadata>>;
+  namespace: string;
+}> {
+  return Promise.resolve({ matches: [], namespace: "" });
+}
+
+function mockPineconeDescribeIndexStats(): Promise<{
+  dimensions: number;
+  numRecords: number;
+}> {
+  return Promise.resolve({ dimensions: 1536, numRecords: 0 });
+}
+
+const mockUpsert = jest.fn();
+const mockQuery = jest.fn().mockImplementation(mockPineconeQuery);
+const mockDescribeIndexStats = jest
+  .fn()
+  .mockImplementation(mockPineconeDescribeIndexStats);
+
+jest.mock("uuid", () => ({
+  ...jest.requireActual("uuid"),
+  v4: jest.fn().mockReturnValue("test-uuid"),
+}));
+
+jest.mock("@pinecone-database/pinecone", () => {
+  const originalModule = jest.requireActual("@pinecone-database/pinecone");
+
+  const index = jest.fn().mockImplementation(() => ({
+    namespace: jest.fn().mockImplementation(() => ({
+      query: mockQuery,
+      upsert: mockUpsert,
+    })),
+    describeIndexStats: mockDescribeIndexStats,
+  }));
+
+  return {
+    ...originalModule,
+    Index: index,
+    Pinecone: jest.fn().mockImplementation(() => ({
+      index,
+    })),
+  };
+});
+
+const mockedPinecone = Pinecone as jest.MockedClass<typeof Pinecone>;
+const mockedGetEnvVar = getEnvVar as jest.MockedFunction<typeof getEnvVar>;
+
+const TEST_CONFIG = {
+  indexName: "test-index",
+  embeddings: new TestEmbeddings(),
+  metadataDB: new InMemoryDocumentMetadataDB(),
+};
+
+describe("Pinecone API key and environment validation", () => {
+  beforeEach(() => {
+    mockedGetEnvVar.mockClear();
+    mockedGetEnvVar.mockImplementation((key) => {
+      if (key === "PINECONE_API_KEY") {
+        return "test-env-api-key";
+      } else if (key === "PINECONE_ENVIRONMENT") {
+        return "test-env-environment";
+      }
+    });
+  });
+
+  test("error thrown if no keys are available", () => {
+    mockedGetEnvVar.mockReturnValue(undefined);
+    expect(() => new PineconeVectorDB(TEST_CONFIG)).toThrowError(
+      "No Pinecone API key found for PineconeVectorDB"
+    );
+
+    expect(
+      () => new PineconeVectorDB({ ...TEST_CONFIG, apiKey: "test-config-key" })
+    ).toThrowError("No Pinecone environment found for PineconeVectorDB");
+  });
+
+  test("uses keys from config when provided", () => {
+    expect(
+      () =>
+        new PineconeVectorDB({
+          ...TEST_CONFIG,
+          apiKey: "test-config-key",
+          environment: "test-config-environment",
+        })
+    ).not.toThrow();
+    expect(mockedPinecone).toHaveBeenCalledWith({
+      apiKey: "test-config-key",
+      environment: "test-config-environment",
+    });
+  });
+
+  test("uses keys from environment when no config keys provided", () => {
+    expect(() => new PineconeVectorDB(TEST_CONFIG)).not.toThrow();
+    expect(mockedPinecone).toHaveBeenCalledWith({
+      apiKey: "test-env-api-key",
+      environment: "test-env-environment",
+    });
+  });
+});
+
+describe("pineconeVectorDB addDocuments", () => {
+  beforeAll(() => {
+    mockedGetEnvVar.mockImplementation((key) => {
+      if (key === "PINECONE_API_KEY") {
+        return "test-env-api-key";
+      } else if (key === "PINECONE_ENVIRONMENT") {
+        return "test-env-environment";
+      }
+    });
+  });
+
+  test("upserts vectors with correct metadata", async () => {
+    const fragment = getTestDocumentFragment({
+      content: "test-fragment-1",
+      metadata: {
+        source: "test-source",
+        stringArray: ["this", "is", "fine"],
+        num: 1,
+        bool: true,
+        nested: {
+          nestedString: "nested",
+          nestedNum: 2,
+          nestedBool: false,
+          nestedStringArray: ["all", "good"],
+          doubleNested: {
+            val: "test",
+          },
+        },
+      },
+    });
+
+    const document = getTestDocument({
+      fragments: [fragment],
+    });
+
+    const documents = [document];
+
+    const vectorDB = new PineconeVectorDB(TEST_CONFIG);
+    await vectorDB.addDocuments(documents);
+    expect(mockUpsert).toHaveBeenCalledWith([
+      {
+        values: TEST_VECTOR,
+        id: "test-uuid",
+        metadata: {
+          source: "test-source",
+          stringArray: ["this", "is", "fine"],
+          num: 1,
+          bool: true,
+          documentId: document.documentId,
+          fragmentId: fragment.fragmentId,
+          "nested.nestedString": "nested",
+          "nested.nestedNum": 2,
+          "nested.nestedBool": false,
+          "nested.nestedStringArray": ["all", "good"],
+          "nested.doubleNested.val": "test",
+          text: "test-fragment-1",
+        },
+      },
+    ]);
+  });
+});
+
+describe("pineconeVectorDB query", () => {});
