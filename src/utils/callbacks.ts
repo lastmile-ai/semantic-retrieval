d8fed710979624621e897d27febe91f092668bdc
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-20 09:32:01 -0400

    [LAS-429][1/n] callbacks for TS flow
    Summary:
    
    The idea is that every object in the flow (e.g. FileSystem) has
    its own particular points at which callbacks may be called, and exposes that
    particular list of points or "events" to the user. The user passes in callbacks as a
    mapping from event name to callback.
    
    A callback is an async (callback_input: object) => void. Return values, online agg,
    and shared state generally not yet supported.
    
    next steps:
    - finish the list of classes / their public methods
    - move things up the object hierarchy and/or out to utils as appropriate.
    
    Testing:
    
    manual: spot check stdout
    npm test callbacks.test.ts

---
+++ src/utils/callbacks.ts
@@ -0,0 +1,75 @@
+import type { RawDocument } from "../document/document";
+
+export type LoadDocumentsSuccessEvent = {
+  name: "onLoadDocumentsSuccess";
+  rawDocuments: RawDocument[];
+};
+
+export type LoadDocumentsErrorEvent = {
+  name: "onLoadDocumentsError";
+  message: string;
+};
+
+type DataSourceEventData = LoadDocumentsSuccessEvent | LoadDocumentsErrorEvent;
+
+type CallbackEvent = DataSourceEventData; // | other stuff
+
+type Callback<T extends CallbackEvent> = (
+  event: T,
+  runId: string
+) => Promise<void>;
+
+// type CallbackMapping = { [key: string]: Callback[] }
+interface CallbackMapping {
+  onLoadDocumentsSuccess?: Callback<LoadDocumentsSuccessEvent>[];
+  onLoadDocumentsError?: Callback<LoadDocumentsErrorEvent>[];
+}
+
+const DEFAULT_CALLBACKS: CallbackMapping = {
+  onLoadDocumentsSuccess: [],
+};
+
+class CallbackManager {
+  // Properties
+  runId: string;
+  callbacks: CallbackMapping;
+
+  // Constructor
+  constructor(runId: string, callbacks: CallbackMapping) {
+    this.runId = runId;
+    this.callbacks = callbacks;
+  }
+
+  async runCallbacks(event: CallbackEvent) {
+    switch (event.name) {
+      case "onLoadDocumentsSuccess":
+        return await this.callback_helper(
+          event,
+          this.callbacks.onLoadDocumentsSuccess,
+          DEFAULT_CALLBACKS.onLoadDocumentsSuccess
+        );
+      case "onLoadDocumentsError":
+        return await this.callback_helper(
+          event,
+          this.callbacks.onLoadDocumentsError,
+          DEFAULT_CALLBACKS.onLoadDocumentsError
+        );
+      default:
+        assertUnreachable(event);
+    }
+  }
+
+  private async callback_helper<T extends CallbackEvent>(
+    event: T,
+    userCallbacks?: Callback<T>[],
+    defaultCallbacks?: Callback<T>[]
+  ) {
+    const allCallbacks = (userCallbacks || []).concat(defaultCallbacks || []);
+    for (const callback of allCallbacks) {
+      // TODO return
+      await callback(event, this.runId);
+    }
+  }
+}
+
+export { CallbackManager, CallbackMapping };
