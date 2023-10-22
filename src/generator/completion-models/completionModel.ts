0e12899a9104cebfb714348486a12106c4d96269
Ryan Holinshead <>
2023-10-22 13:06:50 -0400

    Refactoring Generator

---
+++ src/generator/completion-models/completionModel.ts
@@ -0,0 +1,15 @@
+import { IPrompt } from "../../prompts/prompt";
+
+export interface CompletionModelParams<P> {
+  prompt: string | IPrompt;
+  model?: string;
+  completionParams?: P;
+}
+
+/**
+ * A simple class for interacting with different LLM completion models. CompletionModels
+ * are leveraged by CompletionGenerators to generate completions from prompts.
+ */
+export abstract class CompletionModel<P, R> {
+  abstract run(params: CompletionModelParams<P>): Promise<R>;
+}
