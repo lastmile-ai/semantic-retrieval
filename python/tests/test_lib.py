e2c365e4ec3ee57fbc459808b0555bc41a6b00b6
Suyog Sonwalkar <suyog@lastmileai.dev>
2023-10-22 00:11:39 -0400

    Starting stubs in python (RFC)
    Just starting stub structure (following typescript classes & the demo in `localFileIngestion.ts` for now, can discuss if this makes sense to have structure in place, then can see about splitting implementation).

--- python/tests/test_lib.py
+++ python/tests/test_lib.py
@@ -1,6 +1,11 @@
-from semantic_retrieval import hello_semantic_retrieval
+from semantic_retrieval.ingestion.data_sources.fs.file_system import FileSystem
 
 
-def test_hello_semantic_retrieval():
-    hello_semantic_retrieval()
+def test_create_index():
+    file_system = FileSystem("./example_docs")
+    raw_documents = file_system.load_documents()
+
+    # TODO: Continue making stubs and essentially getting the demo as a test case (similar to localFileIngestion.ts right now)
+    # Then can start to write the actual implementation / split the work
+
     assert True
