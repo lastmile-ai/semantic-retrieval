6906dd09137f0455e6ff96ff0e98bfc0db1f036b
Ryan Holinshead <>
2023-10-22 13:38:04 -0400

    Get localFileIngestion Script Running

--- src/ingestion/document-parsers/parserRegistry.ts
+++ src/ingestion/document-parsers/parserRegistry.ts
@@ -1,6 +1,5 @@
 import { DirectDocumentParser } from "./directDocumentParser";
 import { DocumentParser } from "./documentParser";
-import { TextDocumentParser } from "./textDocumentParser";
 
 /**
  * A registry of document parsers, keyed by MIME type. By default, DirectDocumentParser
@@ -19,7 +18,7 @@ export class ParserRegistry {
     this.parsers =
       parsers ??
       new Map([
-        ["text/plain", new TextDocumentParser()],
+        // ["text/plain", new TextDocumentParser()],
         // ['text/html', new HtmlDocumentParser()],
         // ['application/pdf', new PDFDocumentParser()],
       ]);
