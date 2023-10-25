9236eb5c5cc09c2443e68f87c60e7e0bc3f18774
Suyog Sonwalkar <suyog@lastmileai.dev>
2023-10-25 08:20:12 -0400

    Additional stubs for Python (2/n)

--- python/src/semantic_retrieval/document/document.py
+++ python/src/semantic_retrieval/document/document.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
 
+from semantic_retrieval.common.base import Attributable
+from semantic_retrieval.common.storage import BlobIdentifier
+from enum import Enum
+from typing import Optional
+
 
 class RawDocumentChunk(ABC):
     content: str
@@ -8,8 +13,13 @@ class RawDocumentChunk(ABC):
 
 class RawDocument(ABC):
     url: str
+    data_source: any  # TODO: Update this to DataSource type when it is defined
     name: str
+    mime_type: str
+    hash: str | None
+    blob_id: BlobIdentifier | None
     document_id: str
+    collection_id: str | None
 
     @abstractmethod
     def get_content() -> str:
@@ -18,3 +28,48 @@ class RawDocument(ABC):
     @abstractmethod
     def get_chunked_content() -> list[RawDocumentChunk]:
         return []
+
+
+class DocumentFragmentType(Enum):
+    TEXT = "text"
+    IMAGE = "image"
+    TABLE = "table"
+    LIST = "list"
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+
+
+class DocumentFragment(Attributable):
+    fragment_id: str
+    hash: str | None
+    blob_id: BlobIdentifier | None
+    fragment_type: DocumentFragmentType
+    document_id: str
+    previous_fragment: Optional["DocumentFragment"]
+    next_fragment: Optional["DocumentFragment"]
+    children: Optional[list["DocumentFragment"]]
+
+    @abstractmethod
+    def get_content() -> str:
+        return "Not Implemented"
+
+    @abstractmethod
+    def serialize() -> str:
+        return "Not Implemented"
+
+
+class Document(Attributable):
+    document_id: str
+    collection_id: str | None
+
+    fragments: list[DocumentFragment]
+
+    @abstractmethod
+    def serialize(self) -> str:
+        pass
+
+
+class IngestedDocument(Document):
+    raw_document: RawDocument
