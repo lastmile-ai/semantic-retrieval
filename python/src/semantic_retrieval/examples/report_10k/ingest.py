7201dd789e57cbce068aadb2c554ea8c6f181f42
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-25 17:35:04 -0400

    [LAS-434][1/n][SR-py] barebones retrieval

---
+++ python/src/semantic_retrieval/examples/report_10k/ingest.py
@@ -0,0 +1,28 @@
+import sys
+import numpy as np
+import openai
+
+openai.api_key_path = "/Users/jonathan/keys/dev_OPENAI_API_KEY.txt"
+
+
+def simple_chunk(text, max_chunk_size, stride):
+    out = []
+    i = 0
+    while i < len(text):
+        out.append(text[i : i + max_chunk_size])
+        i += stride
+
+    return out
+
+
+def get_raw_data():
+    return "This is a test of the emergency broadcast system. This is only a test."
+    with open("../examples/demo_data/10k/10k-meta-plain-text-ascii.txt") as f_10k:
+        print(f_10k.read())
+
+
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    return np.array(
+        openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
+    )
