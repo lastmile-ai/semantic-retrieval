a9bded20f9e495cfe7b0d905cd0dbced9f027bdf
Ryan Holinshead <>
2023-10-25 18:37:37 -0400

    Support Persisting InMemoryDocumentMetadataDB to Disk

---
+++ __tests__/document/inMemoryDocumentMetadataDB.test.ts
@@ -0,0 +1,67 @@
+import { InMemoryDocumentMetadataDB } from "../../src/document/metadata/inMemoryDocumentMetadataDB";
+import {
+  getTestDocument,
+  getTestRawDocument,
+} from "../__utils__/testDocumentUtils";
+
+const TEST_DOCUMENT_ID = "test-document-id";
+const TEST_METADATA = {
+  documentId: TEST_DOCUMENT_ID,
+  rawDocument: getTestRawDocument({ documentId: TEST_DOCUMENT_ID }),
+  document: getTestDocument({ documentId: TEST_DOCUMENT_ID }),
+  collectionId: "test-collection-id",
+  uri: "test-document-uri",
+};
+
+describe("inMemoryDocumentMetadataDB", () => {
+  test("get metadata", async () => {
+    const metadataDB = new InMemoryDocumentMetadataDB({
+      [TEST_DOCUMENT_ID]: TEST_METADATA,
+    });
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toBe(TEST_METADATA);
+  });
+
+  test("set metadata", async () => {
+    const metadataDB = new InMemoryDocumentMetadataDB();
+    await metadataDB.setMetadata(TEST_DOCUMENT_ID, TEST_METADATA);
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toBe(TEST_METADATA);
+  });
+
+  test("set metadata overrides existing metadata", async () => {
+    const metadataDB = new InMemoryDocumentMetadataDB({
+      [TEST_DOCUMENT_ID]: TEST_METADATA,
+    });
+
+    await metadataDB.setMetadata(TEST_DOCUMENT_ID, {
+      ...TEST_METADATA,
+      collectionId: "new-collection-id",
+    });
+
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toEqual({
+      ...TEST_METADATA,
+      collectionId: "new-collection-id",
+    });
+  });
+
+  test("persist to, and load from CSV", async () => {
+    const metadataDB = new InMemoryDocumentMetadataDB({
+      [TEST_DOCUMENT_ID]: TEST_METADATA,
+    });
+
+    await metadataDB.persist(
+      "__tests__/__mocks__/document/testDocumentMetadataDB.csv"
+    );
+
+    const metadataDBFromCSV = await InMemoryDocumentMetadataDB.fromCSV(
+      "__tests__/__mocks__/document/testDocumentMetadataDB.csv"
+    );
+
+    const retrievedMetadata =
+      await metadataDBFromCSV.getMetadata(TEST_DOCUMENT_ID);
+
+    expect(retrievedMetadata.documentId).toBe(TEST_DOCUMENT_ID);
+    expect(retrievedMetadata.collectionId).toBe(TEST_METADATA.collectionId);
+    expect(retrievedMetadata.uri).toBe(TEST_METADATA.uri);
+    expect(retrievedMetadata.rawDocument?.uri).toBe("test-source-uri");
+  });
+});
--- src/document/metadata/inMemoryDocumentMetadataDB.ts
+++ src/document/metadata/inMemoryDocumentMetadataDB.ts
@@ -1,5 +1,7 @@
 import { DocumentMetadata } from "./documentMetadata";
 import { DocumentMetadataDB } from "./documentMetadataDB";
+import Papa from "papaparse";
+import fs from "fs/promises";
 
 type DocumentMetadataMap = { [key: string]: DocumentMetadata };
 
@@ -19,5 +21,38 @@ export class InMemoryDocumentMetadataDB implements DocumentMetadataDB {
     metadata: DocumentMetadata
   ): Promise<void> {
     this.metadata[documentId] = metadata;
+  }
+
+  async persist(filePath: string) {
+    const csv = Papa.unparse(
+      Object.entries(this.metadata).map((documentMetadata) => ({
+        documentId: documentMetadata[0],
+        metadata: JSON.stringify(documentMetadata[1], (_key, value) => {
+          // Drop functions from the persisted metadata since they're not useful
+          if (typeof value !== "function") {
+            return value;
+          }
+        }),
+      })),
+      { header: true }
+    );
+    await fs.writeFile(filePath, csv);
+  }
+
+  static async fromCSV(filePath: string): Promise<InMemoryDocumentMetadataDB> {
+    const csvString = await (await fs.readFile(filePath)).toString();
+    const csvData = await Papa.parse<{ documentId: string; metadata: string }>(
+      csvString,
+      { header: true }
+    );
+
+    const metadataMap: DocumentMetadataMap = {};
+
+    for (const row of csvData.data) {
+      const { documentId, metadata } = row;
+      metadataMap[documentId] = JSON.parse(metadata);
+    }
+
+    return new InMemoryDocumentMetadataDB(metadataMap);
   }
 }
