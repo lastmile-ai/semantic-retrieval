d697bd9882a6d5f9f11b9c8bf9f4ed19e39a5f55
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-26 12:39:00 -0400

    [LAS-434][2/n][SR-py] barebones generation
    Summary:
    
    This completes "hello world" RAG in python. Next steps:
    1. make the quality reasonable for the demo reports use case
    2. scale it up to larger set of 10k inputs
      (a) pinecone
      ...
    3. write the real generator logic:
      (a) RBAC simulation
      (b) portfolio lookup
      (C) report structure
    4. eval
    5. refactor into an actual library with test cases
    6. anything I missed?
    
    Test Plan:
    
    Run e2e CLI with a query:
    `python src/semantic_retrieval/examples/report_10k/main.py 'Family of Apps revenue in millions'
    
    Out:
    resp='The context does not provide information on the Family of Apps revenue in millions.'

--- python/src/semantic_retrieval/examples/report_10k/query.py
+++ python/src/semantic_retrieval/examples/report_10k/query.py
@@ -1,16 +1,20 @@
-from functools import partial
-import sys
+from typing import List
 
 import numpy as np
 import openai
-from semantic_retrieval.examples.report_10k.ingest import get_embedding
-from sklearn.metrics.pairwise import cosine_similarity
+
+import semantic_retrieval.common.types as types
+from .ingest import get_embedding
 
 
-def knn(query, candidates, metric_name, k):
+from sklearn.metrics.pairwise import cosine_similarity  # type: ignore
+
+
+def knn(query: str, candidates: types.NPA, metric_name: str, k: int) -> List[int]:
     query_emb = np.expand_dims(get_embedding(query), axis=0)
     if metric_name == "cos_sim":
-        sim_array = np.squeeze(cosine_similarity(query_emb, candidates))
+        cos_sim = cosine_similarity(query_emb, candidates)  # type: ignore
+        sim_array: types.NPA = np.squeeze(cos_sim)
         print(f"{sim_array.shape=}")
         # print(f"{sim_array=}")
         k = min(k, candidates.shape[0])
@@ -21,27 +25,29 @@ def knn(query, candidates, metric_name, k):
         print(f"{top_idxs=}")
 
         # closest first
-        return reversed(top_idxs)
+        return list(reversed(top_idxs))
     else:
         raise NotImplementedError
 
 
-def generate_with_context(query, chunks):
-    # TODO implement
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         {"role": "system", "content": "You are a helpful assistant."},
-    #         {"role": "user", "content": "Who won the world series in 2020?"},
-    #         {
-    #             "role": "assistant",
-    #             "content": "The Los Angeles Dodgers won the World Series in 2020.",
-    #         },
-    #         {"role": "user", "content": "Where was it played?"},
-    #     ],
-    # )
+def generate_with_context(query: str, chunks: List[str]) -> str:
+    system = {
+        "role": "system",
+        "content": (
+            "You are a helpful assistant. "
+            "Rearrange the context to answer the question. "
+            "Do not include any words that do not appear in the context. "
+            "CONTEXT:"
+        )
+        + "\n * ".join(chunks)
+        + "QUESTION:\n",
+    }
+    response = openai.ChatCompletion.create(  # type: ignore
+        model="gpt-4",
+        messages=[
+            system,
+            {"role": "user", "content": query},
+        ],
+    )
 
-    # return response
-
-    # print(f"{response=}")
-    pass
+    return response.choices[0]["message"]["content"]  # type: ignore
