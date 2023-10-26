2c42b58a480268b3f1e9aec64e05fb5a99ba1b38
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-26 13:41:56 -0400

    [LAS-434][2/n][SR-py] barebones generation
    Summary:
    
    This completes "hello world" RAG in python. Next steps:
    1. make the quality reasonable for the demo reports use case
    2. scale it up to larger set of 10k inputs
      (a) pinecone
      ...
    3. write the real generator logic:
      (a) RBAC simulation
      (b) portfolio lookup
      (C) report structure
    4. eval
    5. refactor into an actual library with test cases
    6. anything I missed?
    
    Test Plan:
    
    Run e2e CLI with a query:
    `python src/semantic_retrieval/examples/report_10k/main.py 'Family of Apps revenue in millions'
    
    Out:
    resp='The context does not provide information on the Family of Apps revenue in millions.'

--- python/src/semantic_retrieval/examples/report_10k/ingest.py
+++ python/src/semantic_retrieval/examples/report_10k/ingest.py
@@ -1,12 +1,13 @@
-import sys
+from typing import List
 import numpy as np
+import numpy.typing as npt
 import openai
 
 openai.api_key_path = "/Users/jonathan/keys/dev_OPENAI_API_KEY.txt"
 
 
-def simple_chunk(text, max_chunk_size, stride):
-    out = []
+def simple_chunk(text: str, max_chunk_size: int, stride: int) -> List[str]:
+    out: List[str] = []
     i = 0
     while i < len(text):
         out.append(text[i : i + max_chunk_size])
@@ -16,13 +17,15 @@ def simple_chunk(text, max_chunk_size, stride):
 
 
 def get_raw_data():
-    return "This is a test of the emergency broadcast system. This is only a test."
+    # return "This is a test of the emergency broadcast system. This is only a test."
     with open("../examples/demo_data/10k/10k-meta-plain-text-ascii.txt") as f_10k:
-        print(f_10k.read())
+        # TODO: dont truncate
+        return f_10k.read()[:2000]
 
 
-def get_embedding(text, model="text-embedding-ada-002"):
+def get_embedding(text: str, model: str = "text-embedding-ada-002") -> npt.ArrayLike:
+    # print("get_emb")
     text = text.replace("\n", " ")
-    return np.array(
-        openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
-    )
+    emb: npt.ArrayLike = openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]  # type: ignore
+    out: npt.ArrayLike = np.array(emb)  # type: ignore
+    return out
