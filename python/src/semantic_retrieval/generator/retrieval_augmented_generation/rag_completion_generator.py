0e5d348cdb6b60fba9c5c9d2d3c3d6ed4e26c96d
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-27 14:55:42 -0400

    [LAS-434][4/n][SR-py] fix up top-level API as per financial report example
    Summary:
    
    just do enough so it runs and pyright accepts the demo files
    
    Test plan:
    
    $ python python/src/semantic_retrieval/examples/financial_report/ingest_data.py
    
    NAMESPACE: ns123
    pineconeVectorDB=<semantic_retrieval.data_store.vector_dbs.pinecone_vector_db.PineconeVectorDB object at 0x10583e650>
    
    $ python python/src/semantic_retrieval/examples/financial_report/generate_report.py
    
    Report:
    
    FinancialReport()
    
    $ pyright python/src/semantic_retrieval/examples/financial_report
    
    0 errors, 0 warnings, 0 informations

--- python/src/semantic_retrieval/generator/retrieval_augmented_generation/rag_completion_generator.py
+++ python/src/semantic_retrieval/generator/retrieval_augmented_generation/rag_completion_generator.py
@@ -1,28 +1,31 @@
-import asyncio
+from typing import Generic
 
+from attr import dataclass
+from semantic_retrieval.access_control.access_passport import AccessPassport
+from semantic_retrieval.common.types import Q, R, T
 from semantic_retrieval.generator.completion_generator import (
     LLMCompletionGeneratorParams,
     LLMCompletionGenerator,
 )
-
 from semantic_retrieval.prompts.prompt_template import PromptTemplate
+from semantic_retrieval.retrieval.retriever import BaseRetriever
 
 
+@dataclass
 class RAGCompletionGeneratorParams(LLMCompletionGeneratorParams):
-    def __init__(self, retriever, accessPassport=None, ragPromptTemplate=None):
-        self.retriever = retriever
-        self.accessPassport = accessPassport
-        self.ragPromptTemplate = ragPromptTemplate
+    retriever: BaseRetriever
+    accessPassport: AccessPassport
+    ragPromptTemplate: PromptTemplate
 
 
 DEFAULT_RAG_TEMPLATE = "Answer the question based on the context below.\n\nContext:\n\n{{context}}\n\nQuestion: {{prompt}}\n\nAnswer:"
 
 
-class RAGCompletionGenerator(LLMCompletionGenerator):
-    async def get_retrieval_query(self, params):
+class RAGCompletionGenerator(LLMCompletionGenerator, Generic[T]):
+    async def get_retrieval_query(self, params: T) -> Q:
         # Implement this method to construct the query for the underlying retriever
         pass
 
-    async def run(self, params):
+    async def run(self, params: T) -> R:
         # TODO
         pass
