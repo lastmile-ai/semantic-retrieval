0a65b0783aff3aa70df4d3940effef238d61e50e
Suyog Sonwalkar <suyog@lastmileai.dev>
2023-10-27 16:06:14 -0400

    WIP InMemoryDocumentMetadataDB implementation python

--- python/src/semantic_retrieval/document/metadata/in_memory_document_metadata_db.py
+++ python/src/semantic_retrieval/document/metadata/in_memory_document_metadata_db.py
@@ -1,17 +1,38 @@
 from semantic_retrieval.document.metadata.document_metadata import DocumentMetadata
 from semantic_retrieval.document.metadata.document_metadata_db import DocumentMetadataDB
-from typing import Dict, Optional
+from typing import Awaitable, Dict, Optional
+import json
+from json import JSONEncoder
 
 
-class InMemoryDocumentMetadataDB(DocumentMetadataDB):  # type: ignore
-    def __init__(self, metadata: Optional[Dict[str, DocumentMetadata]] = None):  # type: ignore
-        self.metadata = metadata if metadata is not None else {}
+class DocumentMetadataEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, DocumentMetadata):
+            return obj.model_dump()
+        return super().default(obj)
 
-    #
-    async def getMetadata(self, documentId: str) -> DocumentMetadata:  # type: ignore
-        # TODO
-        pass
 
-    async def setMetadata(self, documentId: str, metadata: DocumentMetadata) -> None:  # type: ignore
-        # TODO
-        pass
+class InMemoryDocumentMetadataDB(DocumentMetadataDB):
+    def __init__(self, metadata: Optional[Dict[str, DocumentMetadata]] = None):
+        if metadata:
+            self.metadata = {k: DocumentMetadata(**v) for k, v in metadata.items()}
+        else:
+            self.metadata = {}
+
+    async def get_metadata(self, document_id: str) -> Awaitable[DocumentMetadata]:
+        return self.metadata.get(document_id)
+
+    async def set_metadata(self, document_id: str, metadata: DocumentMetadata) -> None:
+        self.metadata.update({document_id: metadata})
+
+    def persist(self, file_path: str) -> None:
+        with open(file_path, "w") as f:
+            f.write(json.dumps(self.metadata, cls=DocumentMetadataEncoder))
+
+    def from_json_file(file_path):
+        Awaitable[InMemoryDocumentMetadataDB]
+        with open(file_path, "r") as f:
+            metadata = f.read()
+            map = json.loads(metadata)
+            new_obj = InMemoryDocumentMetadataDB(map)
+            return new_obj
