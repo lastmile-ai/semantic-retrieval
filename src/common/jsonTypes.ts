e7ed1ffeed21e6166c27f5d749637f7a3111feb6
Ryan Holinshead <>
2023-10-27 14:29:22 -0400

    CSVRetriever for Structured CSV Data
    # CSVRetriever for Structured CSV Data
    
    Implement a simple CSVRetriever to retrieved structured data from a CSV file for use in the financial report demo. The retriever takes in a file path and primary key name, then retrieves all the rows from the CSV at the file path, keyed by the primary key name
    
    ```
    ryanholinshead@Ryans-MacBook-Pro semantic-retrieval % yarn test csvRetriever.test.ts
    yarn run v1.22.19
    $ NODE_OPTIONS='--experimental-vm-modules' jest --runInBand csvRetriever.test.ts
     PASS  __tests__/retrieval/csvRetriever.test.ts
      csvRetriever for retrieving structured data from CSV
        ✓ throws error if primary key column is missing (5 ms)
        ✓ retrieveData returns correct data from CSV (2 ms)
    
    Test Suites: 1 passed, 1 total
    Tests:       2 passed, 2 total
    Snapshots:   0 total
    Time:        0.571 s, estimated 1 s
    Ran all test suites matching /csvRetriever.test.ts/i.
    ✨  Done in 1.60s.
    ```

--- src/common/jsonTypes.ts
+++ src/common/jsonTypes.ts
@@ -1,7 +1,7 @@
 export type JSONValue = string | number | boolean | JSONObject | JSONArray;
 
 export interface JSONObject {
-  [x: string]: JSONValue | undefined;
+  [x: string]: JSONValue | null | undefined;
 }
 
 export interface JSONArray extends Array<JSONValue> {}
