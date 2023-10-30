d031cb6206ee1d95dc398222d2d88be8e500e8b6
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-30 17:15:46 -0400

    FileSystem python implementation

--- python/tests/test_lib.py
+++ python/tests/test_lib.py
@@ -10,12 +10,14 @@ metadata_db = None  # TODO: Update to InMemoryMetadataDB once it is defined
 
 @pytest.mark.asyncio
 async def test_create_index():
-    file_system = FileSystem("./example_docs")
+    file_system = FileSystem("examples/example_data/financial_report")
     raw_documents = file_system.load_documents()
 
     await MultiDocumentParser().parse_documents(
         raw_documents,
-        parser_config=ParserConfig(metadata_db=metadata_db, access_control_policy_factory=None),
+        parser_config=ParserConfig(
+            metadata_db=metadata_db, access_control_policy_factory=None
+        ),
     )
 
     # TODO: Continue making stubs and essentially getting the demo as a test case (similar to localFileIngestion.ts right now)
