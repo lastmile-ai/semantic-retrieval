08819715fecf281ce4716cd8dad8de8d2fff005b
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-10-31 09:40:57 -0400

    [WIP][SR-py][demo v0] get metadatadb to load from json, and run an empty query from vector db

--- python/src/semantic_retrieval/data_store/vector_dbs/pinecone_vector_db.py
+++ python/src/semantic_retrieval/data_store/vector_dbs/pinecone_vector_db.py
@@ -8,7 +8,11 @@ from semantic_retrieval.data_store.vector_dbs.vector_db import (
 )
 
 from semantic_retrieval.document.document import Document
-from semantic_retrieval.transformation.embeddings.embeddings import VectorEmbedding
+from semantic_retrieval.document.metadata.document_metadata_db import DocumentMetadataDB
+from semantic_retrieval.transformation.embeddings.embeddings import (
+    DocumentEmbeddingsTransformer,
+    VectorEmbedding,
+)
 
 
 class PineconeVectorDBConfig(VectorDBConfig):
@@ -19,13 +23,25 @@ class PineconeVectorDBConfig(VectorDBConfig):
 
 
 class PineconeVectorDB(VectorDB):
-    def __init__(self, config: PineconeVectorDBConfig):
-        # TODO
-        pass
+    def __init__(
+        self,
+        config: PineconeVectorDBConfig,
+        embeddings: DocumentEmbeddingsTransformer,
+        metadata_db: DocumentMetadataDB,
+    ):
+        self.config = config
+        self.embeddings = embeddings
+        self.metadata_db = metadata_db
 
     @classmethod
-    async def from_documents(cls, documents: List[Document], config: PineconeVectorDBConfig):
-        instance = cls(config)
+    async def from_documents(
+        cls,
+        documents: List[Document],
+        config: PineconeVectorDBConfig,
+        embeddings: DocumentEmbeddingsTransformer,
+        metadata_db: DocumentMetadataDB,
+    ):
+        instance = cls(config, embeddings, metadata_db)
         await instance.add_documents(documents)
         return instance
 
