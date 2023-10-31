af22ab693c3880046b8bb26871910f8a7b4e8077
Ryan Holinshead <>
2023-10-31 14:44:43 -0400

    Support Querying Document IDs by String MetadataDB Fields
    # Support Querying Document IDs by String MetadataDB Fields
    
    Expand querying support in metadata DB to allow querying by any of the string property name/values (uri, name, etc.); needed for next PR where the retriever will query by name
    
    ```
    ryanholinshead@Ryans-MacBook-Pro semantic-retrieval % yarn test inMemoryDocumentMetadataDB.test.ts
    yarn run v1.22.19
    $ NODE_OPTIONS='--experimental-vm-modules' jest --runInBand inMemoryDocumentMetadataDB.test.ts
     PASS  __tests__/document/inMemoryDocumentMetadataDB.test.ts
      inMemoryDocumentMetadataDB
        ✓ get metadata (1 ms)
        ✓ set metadata
        ✓ set metadata overrides existing metadata (1 ms)
        ✓ persist to, and load from JSON file (1 ms)
        ✓ query document ids from metadata
        ✓ query document ids from string fields (1 ms)
    
    Test Suites: 1 passed, 1 total
    Tests:       6 passed, 6 total
    Snapshots:   0 total
    Time:        0.723 s, estimated 1 s
    Ran all test suites matching /inMemoryDocumentMetadataDB.test.ts/i.
    ✨  Done in 2.21s.
    ```

--- src/document/metadata/documentMetadataDB.ts
+++ src/document/metadata/documentMetadataDB.ts
@@ -1,15 +1,35 @@
 import { DocumentMetadata } from "./documentMetadata";
 
 export type DocumentMetadataQuery = {
+  type: "metadata";
   metadataKey: string;
   metadataValue: string;
   matchType: "exact" | "includes";
 };
+
+export type DocumentStringFieldQuery = {
+  type: "string_field";
+  fieldName: keyof Omit<
+    DocumentMetadata,
+    | "document"
+    | "rawDocument"
+    | "dataSource"
+    | "accessPolicies"
+    | "attributes"
+    | "metadata"
+  >;
+  fieldValue: string;
+  matchType: "exact" | "includes";
+};
+
+export type DocumentMetadataDBQuery =
+  | DocumentMetadataQuery
+  | DocumentStringFieldQuery;
 
 export interface DocumentMetadataDB {
   // TODO: saqadri - implement a Postgres implementation of this interface.
   getMetadata(documentId: string): Promise<DocumentMetadata>;
   setMetadata(documentId: string, metadata: DocumentMetadata): Promise<void>;
 
-  queryDocumentIds(query: DocumentMetadataQuery): Promise<string[]>;
+  queryDocumentIds(query: DocumentMetadataDBQuery): Promise<string[]>;
 }
