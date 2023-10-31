c5ebb02e547c2f3c03ce726326619669e6528ab8
Suyog Sonwalkar <suyog@lastmileai.dev>
2023-10-30 23:57:51 -0400

    WIP SeparatorTextChunker Impl

--- python/src/semantic_retrieval/transformation/document/text/separator_text_chunker.py
+++ python/src/semantic_retrieval/transformation/document/text/separator_text_chunker.py
@@ -9,6 +9,7 @@ from semantic_retrieval.transformation.document.text.text_chunk_transformer impo
 
 class SeparatorTextChunkConfig(TextChunkConfig):
     separator: str = " "
+    strip_new_lines: bool = True
 
 
 class SeparatorTextChunkerParams(TextChunkTransformerParams):
@@ -17,12 +18,21 @@ class SeparatorTextChunkerParams(TextChunkTransformerParams):
 
 class SeparatorTextChunker(TextChunkTransformer):
     separator: str = " "  # e.g. words
+    strip_new_lines: bool = True
 
     def __init__(self, params: SeparatorTextChunkerParams):
         super().__init__(params)
-        # TODO
+        # TODO: Impl
         pass
 
     async def chunk_text(self, text: str) -> List[str]:
-        # TODO impl
-        return []
+        text_to_chunk = text
+        if self.strip_new_lines:
+            text_to_chunk = text_to_chunk.replace("\n", " ")
+
+        sub_chunks = self.sub_chunk_on_separator(text_to_chunk, self.separator)
+        merged_chunks = await self.merge_sub_chunks(sub_chunks, self.separator)
+
+        # TODO: Callback / events
+
+        return merged_chunks
