317e6121433bc61b381c0622f88a17ffaf246bb1
Ryan Holinshead <>
2023-10-31 21:28:44 -0400

    Support Querying Document IDs by String MetadataDB Fields
    # Support Querying Document IDs by String MetadataDB Fields
    
    Expand querying support in metadata DB to allow querying by any of the string property name/values (uri, name, etc.); needed for next PR where the retriever will query by name
    
    ```
    ryanholinshead@Ryans-MacBook-Pro semantic-retrieval % yarn test inMemoryDocumentMetadataDB.test.ts
    yarn run v1.22.19
    $ NODE_OPTIONS='--experimental-vm-modules' jest --runInBand inMemoryDocumentMetadataDB.test.ts
     PASS  __tests__/document/inMemoryDocumentMetadataDB.test.ts
      inMemoryDocumentMetadataDB
        ✓ get metadata (1 ms)
        ✓ set metadata
        ✓ set metadata overrides existing metadata (1 ms)
        ✓ persist to, and load from JSON file (1 ms)
        ✓ query document ids from metadata
        ✓ query document ids from string fields (1 ms)
    
    Test Suites: 1 passed, 1 total
    Tests:       6 passed, 6 total
    Snapshots:   0 total
    Time:        0.723 s, estimated 1 s
    Ran all test suites matching /inMemoryDocumentMetadataDB.test.ts/i.
    ✨  Done in 2.21s.
    ```

--- __tests__/document/inMemoryDocumentMetadataDB.test.ts
+++ __tests__/document/inMemoryDocumentMetadataDB.test.ts
@@ -6,13 +6,15 @@ import {
 } from "../__utils__/testDocumentUtils";
 import { AlwaysAllowAccessPolicy } from "../../src/access-control/policies/alwaysAllowAccessPolicy";
 
-const TEST_DOCUMENT_ID = "test-document-id";
-const TEST_METADATA: DocumentMetadata = {
-  documentId: TEST_DOCUMENT_ID,
-  rawDocument: getTestRawDocument({ documentId: TEST_DOCUMENT_ID }),
-  document: getTestDocument({ documentId: TEST_DOCUMENT_ID }),
+const TEST_DOCUMENT_ID_1 = "test-document-id-1";
+const TEST_DOCUMENT_ID_2 = "test-document-id-2";
+
+const TEST_METADATA_1: DocumentMetadata = {
+  documentId: TEST_DOCUMENT_ID_1,
+  rawDocument: getTestRawDocument({ documentId: TEST_DOCUMENT_ID_1 }),
+  document: getTestDocument({ documentId: TEST_DOCUMENT_ID_1 }),
   collectionId: "test-collection-id",
-  uri: "test-document-uri",
+  uri: "test-document-1-uri",
   accessPolicies: [new AlwaysAllowAccessPolicy()],
   metadata: {
     key1: "value1",
@@ -20,39 +22,60 @@ const TEST_METADATA: DocumentMetadata = {
   },
 };
 
+const TEST_METADATA_2: DocumentMetadata = {
+  documentId: TEST_DOCUMENT_ID_2,
+  rawDocument: getTestRawDocument({ documentId: TEST_DOCUMENT_ID_2 }),
+  document: getTestDocument({ documentId: TEST_DOCUMENT_ID_2 }),
+  uri: "test-document-2-uri",
+  metadata: {
+    key1: "value A",
+    key2: "value2",
+    key3: "Look for random value here",
+  },
+};
+
+const QUERY_METADATA_DB = new InMemoryDocumentMetadataDB({
+  [TEST_DOCUMENT_ID_1]: TEST_METADATA_1,
+  [TEST_DOCUMENT_ID_2]: TEST_METADATA_2,
+});
+
 describe("inMemoryDocumentMetadataDB", () => {
   test("get metadata", async () => {
     const metadataDB = new InMemoryDocumentMetadataDB({
-      [TEST_DOCUMENT_ID]: TEST_METADATA,
+      [TEST_DOCUMENT_ID_1]: TEST_METADATA_1,
     });
-    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toBe(TEST_METADATA);
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID_1)).toBe(
+      TEST_METADATA_1
+    );
   });
 
   test("set metadata", async () => {
     const metadataDB = new InMemoryDocumentMetadataDB();
-    await metadataDB.setMetadata(TEST_DOCUMENT_ID, TEST_METADATA);
-    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toBe(TEST_METADATA);
+    await metadataDB.setMetadata(TEST_DOCUMENT_ID_1, TEST_METADATA_1);
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID_1)).toBe(
+      TEST_METADATA_1
+    );
   });
 
   test("set metadata overrides existing metadata", async () => {
     const metadataDB = new InMemoryDocumentMetadataDB({
-      [TEST_DOCUMENT_ID]: TEST_METADATA,
+      [TEST_DOCUMENT_ID_1]: TEST_METADATA_1,
     });
 
-    await metadataDB.setMetadata(TEST_DOCUMENT_ID, {
-      ...TEST_METADATA,
+    await metadataDB.setMetadata(TEST_DOCUMENT_ID_1, {
+      ...TEST_METADATA_1,
       collectionId: "new-collection-id",
     });
 
-    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID)).toEqual({
-      ...TEST_METADATA,
+    expect(await metadataDB.getMetadata(TEST_DOCUMENT_ID_1)).toEqual({
+      ...TEST_METADATA_1,
       collectionId: "new-collection-id",
     });
   });
 
   test("persist to, and load from JSON file", async () => {
     const metadataDB = new InMemoryDocumentMetadataDB({
-      [TEST_DOCUMENT_ID]: TEST_METADATA,
+      [TEST_DOCUMENT_ID_1]: TEST_METADATA_1,
     });
 
     await metadataDB.persist(
@@ -64,60 +87,83 @@ describe("inMemoryDocumentMetadataDB", () => {
     );
 
     const retrievedMetadata =
-      await metadataDBFromJSON.getMetadata(TEST_DOCUMENT_ID);
+      await metadataDBFromJSON.getMetadata(TEST_DOCUMENT_ID_1);
 
-    expect(retrievedMetadata.documentId).toBe(TEST_DOCUMENT_ID);
-    expect(retrievedMetadata.collectionId).toBe(TEST_METADATA.collectionId);
-    expect(retrievedMetadata.uri).toBe(TEST_METADATA.uri);
+    expect(retrievedMetadata.documentId).toBe(TEST_DOCUMENT_ID_1);
+    expect(retrievedMetadata.collectionId).toBe(TEST_METADATA_1.collectionId);
+    expect(retrievedMetadata.uri).toBe(TEST_METADATA_1.uri);
     expect(retrievedMetadata.rawDocument?.uri).toBe("test-source-uri");
     expect(retrievedMetadata.accessPolicies).toEqual(
-      TEST_METADATA.accessPolicies
+      TEST_METADATA_1.accessPolicies
     );
   });
 
   test("query document ids from metadata", async () => {
-    const testDocumentID2 = "test-document-id-2";
-    const metadataDB = new InMemoryDocumentMetadataDB({
-      [TEST_DOCUMENT_ID]: TEST_METADATA,
-      [testDocumentID2]: {
-        documentId: testDocumentID2,
-        rawDocument: getTestRawDocument({ documentId: testDocumentID2 }),
-        document: getTestDocument({ documentId: testDocumentID2 }),
-        uri: "test-document-2-uri",
-        metadata: {
-          key1: "value A",
-          key2: "value2",
-          key3: "Look for random value here",
-        },
-      },
-    });
-
     expect(
-      await metadataDB.queryDocumentIds({
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "metadata",
         metadataKey: "key2",
         metadataValue: "value2",
         matchType: "exact",
       })
-    ).toEqual([TEST_DOCUMENT_ID, testDocumentID2]);
+    ).toEqual([TEST_DOCUMENT_ID_1, TEST_DOCUMENT_ID_2]);
     expect(
-      await metadataDB.queryDocumentIds({
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "metadata",
         metadataKey: "key1",
         metadataValue: "value A",
         matchType: "exact",
       })
-    ).toEqual([testDocumentID2]);
+    ).toEqual([TEST_DOCUMENT_ID_2]);
     expect(
-      await metadataDB.queryDocumentIds({
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "metadata",
         metadataKey: "key3",
         metadataValue: "random",
         matchType: "includes",
       })
-    ).toEqual([testDocumentID2]);
+    ).toEqual([TEST_DOCUMENT_ID_2]);
     expect(
-      await metadataDB.queryDocumentIds({
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "metadata",
         metadataKey: "key3",
         metadataValue: "random",
         matchType: "exact",
+      })
+    ).toEqual([]);
+  });
+
+  test("query document ids from string fields", async () => {
+    expect(
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "string_field",
+        fieldName: "uri",
+        fieldValue: "test-document-2-uri",
+        matchType: "exact",
+      })
+    ).toEqual([TEST_DOCUMENT_ID_2]);
+    expect(
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "string_field",
+        fieldName: "uri",
+        fieldValue: "test-document",
+        matchType: "includes",
+      })
+    ).toEqual([TEST_DOCUMENT_ID_1, TEST_DOCUMENT_ID_2]);
+    expect(
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "string_field",
+        fieldName: "collectionId",
+        fieldValue: "test",
+        matchType: "includes",
+      })
+    ).toEqual([TEST_DOCUMENT_ID_1]);
+    expect(
+      await QUERY_METADATA_DB.queryDocumentIds({
+        type: "string_field",
+        fieldName: "uri",
+        fieldValue: "test-dne",
+        matchType: "includes",
       })
     ).toEqual([]);
   });
