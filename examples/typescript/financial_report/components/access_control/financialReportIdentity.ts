630f77173a8fa7118bb5b83e6458c2ea83e26f4c
Ryan Holinshead <>
2023-11-01 11:58:30 -0400

    update_access_control Script & Policy

--- examples/typescript/financial_report/components/access_control/financialReportIdentity.ts
+++ examples/typescript/financial_report/components/access_control/financialReportIdentity.ts
@@ -3,5 +3,10 @@ import { AccessIdentity } from "../../../../../src/access-control/accessIdentity
 export interface FinancialReportIdentity extends AccessIdentity {
   resource: "financial_data";
   role: "user" | "admin";
-  client?: string;
+}
+
+export function isFinancialReportIdentity(
+  identity: AccessIdentity
+): identity is FinancialReportIdentity {
+  return identity.resource === "financial_data";
 }
