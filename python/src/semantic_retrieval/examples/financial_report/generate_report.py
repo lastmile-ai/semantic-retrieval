706110d6d8e7c26886ac77a7b638c08927413897
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-05 17:20:29 -0500

    [SR-py] RBAC for PC and update script

--- python/src/semantic_retrieval/examples/financial_report/generate_report.py
+++ python/src/semantic_retrieval/examples/financial_report/generate_report.py
@@ -1,4 +1,5 @@
 import asyncio
+from functools import partial
 import json
 import logging
 import os
@@ -11,6 +12,8 @@ from semantic_retrieval.access_control.access_function import AccessFunction
 from semantic_retrieval.access_control.access_identity import AuthenticatedIdentity
 
 from semantic_retrieval.common.core import LOGGER_FMT, file_contents
+from semantic_retrieval.document.metadata.document_metadata import DocumentMetadata
+from semantic_retrieval.document.metadata.document_metadata_db import DocumentMetadataDB
 
 import semantic_retrieval.examples.financial_report.financial_report_generator as frg
 from result import Err, Ok, Result
@@ -69,9 +72,9 @@ async def run_generate_report(config: Config):
             print(f"Error loading metadataDB: {msg}")
             return -1
         case Ok(metadata_db):
-            openaiembcfg = OpenAIEmbeddingsConfig(api_key=config.openai_key)
+            openai_embedding_config = OpenAIEmbeddingsConfig(api_key=config.openai_key)
 
-            pcvdbcfg = PineconeVectorDBConfig(
+            pinecone_vectordb_config = PineconeVectorDBConfig(
                 index_name=config.index_name,
                 namespace=config.namespace,
                 api_key=config.pinecone_key,
@@ -103,32 +106,40 @@ async def run_generate_report(config: Config):
                 case Ok(df_portfolio):
                     report = await _generate_report_for_portfolio(
                         df_portfolio,
-                        pcvdbcfg=pcvdbcfg,
-                        openaiembcfg=openaiembcfg,
+                        pinecone_vectordb_config=pinecone_vectordb_config,
+                        openai_embedding_config=openai_embedding_config,
                         metadata_db=metadata_db,
                         config=config,
+                        viewer_identity=viewer_identity,
                     )
 
                     # TODO [P1]: Save res to disk
                     print("Report:\n")
-                    print(str(report))
+                    print(report.unwrap_or_else(lambda err: f"Error: {err}"))
 
 
 async def _generate_report_for_portfolio(
     df_portfolio: pd.DataFrame,
-    pcvdbcfg: PineconeVectorDBConfig,
-    openaiembcfg: OpenAIEmbeddingsConfig,
+    pinecone_vectordb_config: PineconeVectorDBConfig,
+    openai_embedding_config: OpenAIEmbeddingsConfig,
     metadata_db: InMemoryDocumentMetadataDB,
+    viewer_identity: AuthenticatedIdentity,
     config: Config,
 ) -> Result[str, str]:
     portfolio = portfolio_df_to_dict(df_portfolio)
     logger.info("\nPortfolio:\n" + json.dumps(portfolio, indent=2))
 
-    retriever = FinancialReportDocumentRetriever(
-        vector_db_config=pcvdbcfg,
-        embeddings_config=openaiembcfg,
-        portfolio=portfolio,  # type: ignore [fixme]
+    access_function_10k: AccessFunction = partial(
+        validate_10k_access,
         metadata_db=metadata_db,
+    )
+    retriever = FinancialReportDocumentRetriever(
+        vector_db_config=pinecone_vectordb_config,
+        embeddings_config=openai_embedding_config,
+        portfolio=portfolio,
+        metadata_db=metadata_db,
+        viewer_identity=viewer_identity,
+        user_access_function=access_function_10k,
     )
 
     generator = FinancialReportGenerator()
@@ -168,7 +179,7 @@ def portfolio_df_to_dict(df: pd.DataFrame) -> PortfolioData:
 
 async def validate_portfolio_access(resource_auth_id: str, viewer_auth_id: str) -> bool:
     # In production, this function could do an IAM lookup, DB access, etc.
-    # For this simulatio, we read from a local JSON file.
+    # For this simulation, we read from a local JSON file.
 
     # In this case, the resource_auth_id is the csv path
     # and the viewer_auth_id is the advisor name.
@@ -184,6 +195,43 @@ async def validate_portfolio_access(resource_auth_id: str, viewer_auth_id: str) 
     db_iam_simulation = json.loads(file_contents(path))["advisors"]
 
     return db_iam_simulation.get(client_name, None) == viewer_auth_id
+
+
+async def validate_10k_access(
+    resource_auth_id: str, viewer_auth_id: str, metadata_db: DocumentMetadataDB
+) -> bool:
+    def _validate_10k_access_with_metadata(
+        resource_auth_id: str,
+        viewer_auth_id: str,
+        metadata: DocumentMetadata,
+    ):
+        uri = metadata.uri
+        ticker_re = re.search(r".*_([A-Z\.]+)\..*", uri)
+        if not ticker_re:
+            return False
+        ticker = str(ticker_re.groups()[0])
+        # if ticker_re else ""
+        # print(f"{out=}")
+
+        logger.debug(
+            f"validate_10k_access({resource_auth_id=}, {viewer_auth_id=}, {ticker=}"
+        )
+        path = "python/src/semantic_retrieval/examples/financial_report/access_control/iam_simulation_db.json"
+        db_iam_simulation = json.loads(file_contents(path))["access_10ks"]
+        return ticker in db_iam_simulation.get(viewer_auth_id, [])
+
+    res_metadata = await metadata_db.get_metadata(resource_auth_id)
+
+    match res_metadata:
+        case Err(_msg):
+            # TODO log
+            return False
+        case Ok(metadata):
+            return _validate_10k_access_with_metadata(
+                metadata=metadata,
+                viewer_auth_id=viewer_auth_id,
+                resource_auth_id=resource_auth_id,
+            )
 
 
 if __name__ == "__main__":
