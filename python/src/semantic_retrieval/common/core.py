689668235f844787e747a33bc5133ea29cfa3fec
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-07 11:27:02 -0500

    [wip] v2 make ingest fast

--- python/src/semantic_retrieval/common/core.py
+++ python/src/semantic_retrieval/common/core.py
@@ -1,10 +1,13 @@
 import logging
+from typing import Iterable, List, TypeVar
 
 
 LOGGER_FMT = "[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d: %(message)s"
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format=LOGGER_FMT)
+
+T = TypeVar("T")
 
 
 def file_contents(path: str):
@@ -25,3 +28,17 @@ def text_file_write(path: str, contents: str):
         # TODO [P1] deal with this
         logger.critical("exn=" + str(e))
         return ""
+
+
+def flatten_list(list_of_lists: List[List[T]]) -> List[T]:
+    return [item for sublist in list_of_lists for item in sublist]
+
+
+def unflatten_iterable(it: Iterable[T], chunk_size: int) -> List[List[T]]:
+    out = [[]]
+    for x in it:
+        if len(out[-1]) == chunk_size:
+            out.append([])
+        out[-1].append(x)
+
+    return out
