3d620e4e0a7f5f63cdacb0dc6ba8cfe7c995810c
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-13 14:09:41 -0500

    [SR-PY] add config-related helper functions

--- python/src/semantic_retrieval/utils/configs/configs.py
+++ python/src/semantic_retrieval/utils/configs/configs.py
@@ -1,5 +1,8 @@
 import argparse
+import os
 from typing import Any, Dict, List, Optional, Set, Type
+
+from dotenv import load_dotenv
 
 from semantic_retrieval.common.types import Record
 
@@ -36,3 +39,38 @@ def argparsify(r: Record | Type[Record], required: Optional[Set[str]] = None):
     parser = argparse.ArgumentParser()
     add_parser_arguments(parser, r.model_fields, required=required)
     return parser
+
+
+def get_config_args(args: argparse.Namespace | Dict[str, Any]) -> Dict[str, Any]:
+    def _get_cli():
+        if isinstance(args, argparse.Namespace):
+            return vars(args)
+        else:
+            return dict(args)
+
+    env_keys = {
+        "openai_key": "OPENAI_API_KEY",
+        "pinecone_key": "PINECONE_API_KEY",
+        "pinecone_environment": "PINECONE_ENVIRONMENT",
+        "pinecone_index_name": "PINECONE_INDEX_NAME",
+        "pinecone_namespace": "PINECONE_NAMESPACE",
+    }
+
+    load_dotenv()
+    env_values = remove_nones({k: os.getenv(v) for k, v in env_keys.items()})
+    cli_values = remove_nones(_get_cli())
+
+    # TODO: generalize combining args
+    all_args = dict(env_values, **cli_values)
+    return all_args
+
+
+def resolve_path(data_root: str, path: str) -> str:
+    """
+    data_root: path relative to cwd where all the data is stored.
+    path: relative to data_root, the specific data to look at,
+    e.g. "10ks/".
+    """
+
+    joined = os.path.join(os.getcwd(), data_root, path)
+    return os.path.abspath(os.path.expanduser(joined))
