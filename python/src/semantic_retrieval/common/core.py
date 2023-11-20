2f93b3580ce42d23ba0a3ef8dda0b6bbf3a5340b
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-20 10:16:32 -0500

    [qol 1/n]: utils
    Summary:
    
    Adding some very fundamental utilities to make development smoother, e.g.
    * safe file I/O
    * abstracted config class construction from CLi
    * Result convenience functions
    etc.

--- python/src/semantic_retrieval/common/core.py
+++ python/src/semantic_retrieval/common/core.py
@@ -1,9 +1,24 @@
+import json
 import logging
+import os
 import time
-from typing import Any, Callable, Iterable, List, Optional, ParamSpec, TypeVar
+from typing import (
+    IO,
+    Any,
+    Callable,
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    ParamSpec,
+    TypeVar,
+    cast,
+)
 
+from jsoncomment import JsonComment
 from result import Err, Ok, Result
-
+from semantic_retrieval.common.json_types import JSONObject
+from semantic_retrieval.functional.functional import ErrWithTraceback
 
 P = ParamSpec("P")
 
@@ -14,10 +29,12 @@ U_Callable = TypeVar("U_Callable", bound=Callable[..., Any])
 
 LOGGER_FMT = "[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d: %(message)s"
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 logging.basicConfig(format=LOGGER_FMT)
 
 T = TypeVar("T")
+T2 = TypeVar("T2")
+U2 = TypeVar("U2")
 U = TypeVar("U")
 
 E = TypeVar("E")
@@ -29,24 +46,50 @@ G = TypeVar("G", bound=Callable[..., Any])
 Decorator = Callable[[F], G]
 
 
-def file_contents(path: str):
+def text_file_read_with_handle(f_handle: IO[str]) -> Result[str, str]:
     try:
-        with open(path, "r") as f:
-            return f.read()
-    except Exception as e:
-        # TODO [P1] deal with this
-        logger.critical("exn=" + str(e))
-        return ""
+        return Ok(f_handle.read())
+    except IOError as e:
+        return ErrWithTraceback(e)
 
 
-def text_file_write(path: str, contents: str):
+def make_text_path_handler(
+    f_handle_fn: Callable[[IO[str]], Result[T, str]],
+    mode: str,
+    encoding: str | None = None,
+) -> Callable[[str | None], Result[T, str]]:
+    encoding = encoding or "utf8"
+
+    def text_path_handler(path: str | None) -> Result[T, str]:
+        if not path:
+            return Err("Path is None")
+        try:
+            with open(path, mode, encoding=encoding) as f:
+                return f_handle_fn(f)
+        except IOError as e:
+            return ErrWithTraceback(e)
+
+    return text_path_handler
+
+
+def file_contents(path: str | None) -> Result[str, str]:
+    path_fn = make_text_path_handler(text_file_read_with_handle, "r")
+    return path_fn(path)
+
+
+def text_file_write(path: str | None, contents: str) -> Result[int, str]:
+    def _write(f_handle: IO[str]) -> Result[int, str]:
+        return text_file_write_with_handle(f_handle, contents)
+
+    path_fn = make_text_path_handler(_write, "w")
+    return path_fn(path)
+
+
+def text_file_write_with_handle(f_handle: IO[str], contents: str) -> Result[int, str]:
     try:
-        with open(path, "w") as f:
-            return f.write(contents)
-    except Exception as e:
-        # TODO [P1] deal with this
-        logger.critical("exn=" + str(e))
-        return -1
+        return Ok(f_handle.write(contents))
+    except IOError as e:
+        return ErrWithTraceback(e)
 
 
 def flatten_list(list_of_lists: List[List[T]]) -> List[T]:
@@ -88,3 +131,121 @@ def exp_backoff(
         return wrapper_thunk
 
     return dec
+
+
+def remove_nones(d: Mapping[str, Any]) -> dict[str, Any]:
+    return {k: v for k, v in d.items() if v is not None}
+
+
+def dict_union_best_effort(
+    *dicts: Mapping[T, Any], on_conflict: Optional[str] = None
+) -> dict[T, Any]:
+    assert on_conflict != "err"
+    res = dict_union(*dicts, on_conflict=on_conflict)
+    match res:
+        case Ok(d):
+            return d
+        case Err(e):
+            assert False, f"should be unreachable: {e=}"
+
+
+def dict_union(
+    *dicts: Mapping[T, Any], on_conflict: Optional[str] = None
+) -> Result[dict[T, Any], str]:
+    # TODO make this an enum
+    on_conflict = on_conflict or "err"
+
+    result = {}
+    for d in dicts:
+        for k, v in d.items():
+            if k not in result or result[k] == v:
+                result[k] = v
+            else:
+                if on_conflict == "err":
+                    return Err(f"Key {k} exists with different values: {v}, {result[k]}.")
+                elif on_conflict == "keep_first":
+                    pass
+                elif on_conflict == "replace":
+                    result[k] = v
+                else:
+                    assert False, f"should be unreachable: invalid {on_conflict=}"
+
+    return Ok(result)
+
+
+def enforce_unique(iterable: Iterable[T]) -> Result[T, str]:
+    iter_ = iter(iterable)
+    value = next(iter_)
+    for x in iter_:
+        if value != x:
+            return Err(f"distinct values: {value}, {x}")
+    return Ok(value)
+
+
+only = enforce_unique
+
+
+def dict_map(
+    d: Mapping[T, U],
+    key_fn: Optional[Callable[[T, U], T2]] = None,
+    value_fn: Optional[Callable[[T, U], U2]] = None,
+) -> dict[T2, U2]:
+    def key_fn_(k: T, v: U) -> T2:
+        if key_fn:
+            return key_fn(k, v)
+        else:
+            return cast(T2, k)
+
+    def value_fn_(k: T, v: U) -> U2:
+        if value_fn:
+            return value_fn(k, v)
+        else:
+            return cast(U2, v)
+
+    return {key_fn_(k, v): value_fn_(k, v) for k, v in d.items()}
+
+
+def dict_invert(d: Mapping[T, U]) -> Result[dict[U, T], str]:
+    out = {}
+    for k, v in d.items():
+        if v in out:
+            return Err(f"Not one-to-one: {k=},{v=}, {out[v]=}")
+        else:
+            out[v] = k
+
+    return Ok(out)
+
+
+def combine_returncodes(returncodes: Iterable[int]) -> int:
+    """
+    Return 0 if all 0 or first non-zero return code
+    """
+    for rc in returncodes:
+        if rc != 0:
+            return rc
+
+    return 0
+
+
+def load_json(json_str: str) -> Result[JSONObject, str]:
+    parser = JsonComment()
+    try:
+        return Ok(parser.loads(json_str))
+    except json.JSONDecodeError as e:
+        return ErrWithTraceback(e)
+
+
+def load_json_file(file_path: str | None) -> Result[JSONObject, str]:
+    contents = file_contents(file_path)
+    return contents.and_then(load_json)
+
+
+def normalize_path(path: str) -> str:
+    return os.path.realpath(path)
+
+
+def deprefix(s: str, pfx: str) -> str:
+    if s.startswith(pfx):  # Checks if the string starts with the given prefix
+        return s[len(pfx) :]  # If true, returns the string without the prefix
+    else:
+        return s
--- python/src/semantic_retrieval/utils/configs/configs.py
+++ python/src/semantic_retrieval/utils/configs/configs.py
@@ -1,47 +1,77 @@
 import argparse
+import logging
 import os
-from typing import Any, Dict, List, Optional, Set, Type
+from enum import EnumMeta
+from types import UnionType
+from typing import Any, Dict, Optional, Set, Type, TypeVar
 
+import pydantic
 from dotenv import load_dotenv
-
+from result import Ok, Result
+from semantic_retrieval.common.core import dict_union_best_effort, remove_nones
+from semantic_retrieval.common.json_types import JSONObject
 from semantic_retrieval.common.types import Record
+from semantic_retrieval.functional.functional import ErrWithTraceback
+
+LOGGER = logging.getLogger(__name__)
 
 
-def remove_nones(d: Dict[str, Any]) -> Dict[str, Any]:
-    return {k: v for k, v in d.items() if v is not None}
-
-
-def combine_dicts(d_list: List[Dict[str, Any]]) -> Dict[str, Any]:
-    """
-    Precedence: highest first
-    """
-    out = {}
-    for d in reversed(d_list):
-        out.update(d)
-
-    return out
-
-
-def add_parser_argument(parser, field_name, field, is_required: bool):  # type: ignore
+def add_parser_argument(
+    parser: argparse.ArgumentParser,
+    field_name: str,
+    field: pydantic.fields.FieldInfo,
+    is_required: bool,
+) -> Optional[str]:
     field_name = field_name.replace("_", "-")
     the_type = field.annotation
-    parser.add_argument(f"--{field_name}", type=the_type, required=is_required)
+    if the_type is None:
+        return f"{field_name}: the_type is None"
+    elif the_type is bool:
+        if is_required:
+            return f"(bool) flag cannot be required: field={field_name}"
+        parser.add_argument(
+            f"--{field_name}",
+            action="store_true",
+        )
+    elif type(the_type) is UnionType:
+        return f"UnionType not supported. field={field_name}"
+    elif isinstance(the_type, EnumMeta):
+        parser.add_argument(
+            f"--{field_name}",
+            type=str,
+            choices=[e.value.lower() for e in the_type],  # type: ignore
+            required=is_required,
+        )
+    else:
+        parser.add_argument(
+            f"--{field_name}", type=the_type, required=is_required
+        )
 
 
-def add_parser_arguments(parser, fields, required: Optional[Set[str]] = None):  # type: ignore
+def add_parser_arguments(
+    parser: argparse.ArgumentParser,
+    fields: dict[str, pydantic.fields.FieldInfo],
+    required: Optional[Set[str]] = None,
+):
     required = required or set()
     for field_name, field in fields.items():
         is_required = field_name in required
-        add_parser_argument(parser, field_name, field, is_required)
+        res = add_parser_argument(parser, field_name, field, is_required)
+        if res is not None:
+            LOGGER.warning(res)
 
 
-def argparsify(r: Record | Type[Record], required: Optional[Set[str]] = None):
+def argparsify(
+    r: Record | Type[Record], required: Optional[Set[str]] = None
+) -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     add_parser_arguments(parser, r.model_fields, required=required)
     return parser
 
 
-def get_config_args(args: argparse.Namespace | Dict[str, Any]) -> Dict[str, Any]:
+def get_config_args(
+    args: argparse.Namespace | Dict[str, Any]
+) -> Dict[str, Any]:
     def _get_cli():
         if isinstance(args, argparse.Namespace):
             return vars(args)
@@ -60,8 +90,9 @@ def get_config_args(args: argparse.Namespace | Dict[str, Any]) -> Dict[str, Any]
     env_values = remove_nones({k: os.getenv(v) for k, v in env_keys.items()})
     cli_values = remove_nones(_get_cli())
 
-    # TODO: generalize combining args
-    all_args = dict(env_values, **cli_values)
+    all_args = dict_union_best_effort(
+        env_values, cli_values, on_conflict="replace"
+    )
     return all_args
 
 
@@ -74,3 +105,24 @@ def resolve_path(data_root: str, path: str) -> str:
 
     joined = os.path.join(os.getcwd(), data_root, path)
     return os.path.abspath(os.path.expanduser(joined))
+
+
+T_Record = TypeVar("T_Record", bound=Record)
+
+
+def parse_args(
+    parser: argparse.ArgumentParser,
+    argv: list[str],
+    config_type: Type[T_Record],
+) -> Result[T_Record, str]:
+    cli_dict = remove_nones(vars(parser.parse_args(argv)))
+    return config_from_primitives(config_type, cli_dict)
+
+
+def config_from_primitives(
+    config_type: Type[T_Record], cli_dict: JSONObject
+) -> Result[T_Record, str]:
+    try:
+        return Ok(config_type.model_validate(cli_dict))
+    except pydantic.ValidationError as e:
+        return ErrWithTraceback(e, "Invalid args")
